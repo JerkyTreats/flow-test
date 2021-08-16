1b966f3aa71af68ab0e3be69e1168d59567f4162
JerkyTreats <justinfnearing@gmail.com>
2021-08-16 16:05:01 -0700

    Show blank CheckAddress result if not pressed

--- src/components/CheckAddress.js
+++ src/components/CheckAddress.js
@@ -7,24 +7,31 @@ import * as fcl from "@onflow/fcl"
 const CheckAddress = () => {
     const [user, setUser ] = useState({loggedIn: null})
     useEffect(() => fcl.currentUser().subscribe(setUser), [])
-    const [ initialized, setInitialized ] = useState(false)
+    const [ initialized, setInitialized ] = useState('unset')
 
     const checkInitialize = (address) => {
+        console.log(address)
         isInitialized(address).then((fclResult => {
+            console.log(fclResult)
             setInitialized(fclResult)
         }))
     }
+
+    const initializedMessage = initialized === 'unset' ? '' : `Initialized: ${initialized.toString()}`
 
     if (user.loggedIn) {
         return (
             <Container>
                 <Button onClick={() => checkInitialize(user.addr) }>Check Address</Button>
-                <span> { initialized.toString() } </span>
+                <span> { initializedMessage } </span>
             </Container>
         )
     } else {
         return (
-            <Button disabled>Check Address </Button>
+            <Container>
+                <Button disabled>Check Address</Button>
+                <span>Login to Continue</span>
+            </Container>
         )
     }
 }
