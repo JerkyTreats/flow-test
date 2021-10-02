d969fdab92b51e8f56c36dc055fcc8c48152713c
JerkyTreats <justinfnearing@gmail.com>
2021-10-01 17:09:51 -0700

    Display Profile data via Flow blockchain query

--- src/pages/Profile.page.js
+++ src/pages/Profile.page.js
@@ -3,16 +3,13 @@ import { Container } from 'react-bootstrap'
 import ReadProfile from '../components/ReadProfile'
 
 const Profile = () => {
-    ReadProfile()
     return (
         <>
         <div className="header-default">
             <h2> Profile </h2>
         </div>
 
-        <Container>
-            {ReadProfile()}
-        </Container>
+        {ReadProfile()}
         </>
     )
 }
